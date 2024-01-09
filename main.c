3c2a1f4e1868de5d7fa2ddb2c6748b1d6b90e915
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-01-09 13:37:53 +0800

    Impl main superblock parsing (needs improvement)

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 #define ROOT_INODE_NUM 2
+#define ADDR_SB 1024
+
+int do_lseek(int fd, int offset, int whence);
 
 // superblock struct.
-struct Superblock
+struct SB
 {
     uint32_t totalInodes;
     uint32_t totalBlocks;
     uint32_t blockSizeMult;
     uint32_t blocksPerBG;
     uint32_t inodesPerBG;
+    uint32_t inodeSize;
 };
 
 int main(int argc, char *argv[])
@@ -20,8 +26,8 @@ int main(int argc, char *argv[])
     // Check if at least one argument is provided
     if (argc < 2)
     {
-        printf("[ERROR] No argument/s provided.\n");
-        return 1;
+        perror("Must provide at least one argument");
+        exit(1);
     }
 
     // Get the ext2 file system file path.
@@ -37,20 +43,67 @@ int main(int argc, char *argv[])
     }
     // -------------------------------------------------------------------------
 
-    // Open the ext2 file system file for reading.
-    FILE *ext2FS = fopen(ext2FP, "r");
-    if (ext2FS == NULL)
+    // Open the ext2 file system for reading (use open not fopen).
+    int fd = open(ext2FP, O_RDONLY);
+    if (fd < 0)
     {
-        printf("[ERROR] Unable to open the ext2 file system.\n");
-        return 1;
+        perror("Failed to open ext2 file system");
+        exit(1);
     }
 
-    // Read the superblock.
-    struct Superblock superblock;
-    fseek(ext2FS, 1024, SEEK_SET);
+    // Print the bytes 1024 to 2047 in hexadecimal.
+    unsigned char buf[1024];
+    do_lseek(fd, 1024, SEEK_SET);
+    for (int i = 0; i < 1024; i++)
+    {
+        read(fd, &buf[i], 1);
+        printf("%02x ", buf[i]);
+    }
+    printf("\n");
+
+    // Read the main superblock.
+    struct SB sb;
+
+    do_lseek(fd, ADDR_SB, SEEK_SET);
+    read(fd, &sb.totalInodes, sizeof(sb.totalInodes));
+
+    do_lseek(fd, ADDR_SB + 4, SEEK_SET);
+    read(fd, &sb.totalBlocks, sizeof(sb.totalBlocks));
+
+    do_lseek(fd, ADDR_SB + 24, SEEK_SET);
+    read(fd, &sb.blockSizeMult, sizeof(sb.blockSizeMult));
+
+    do_lseek(fd, ADDR_SB + 32, SEEK_SET);
+    read(fd, &sb.blocksPerBG, sizeof(sb.blocksPerBG));
+
+    do_lseek(fd, ADDR_SB + 40, SEEK_SET);
+    read(fd, &sb.inodesPerBG, sizeof(sb.inodesPerBG));
+
+    do_lseek(fd, ADDR_SB + 88, SEEK_SET);
+    read(fd, &sb.inodeSize, sizeof(sb.inodeSize));
+
+    // Print the superblock information.
+    printf("totalInodes: %d\n", sb.totalInodes);
+    printf("totalBlocks: %d\n", sb.totalBlocks);
+    printf("blockSizeMult: %d\n", sb.blockSizeMult);
+    printf("blocksPerBG: %d\n", sb.blocksPerBG);
+    printf("inodesPerBG: %d\n", sb.inodesPerBG);
+    printf("inodeSize: %d\n", sb.inodeSize);
 
     // Close the ext2 file system.
-    fclose(ext2FS);
+    close(fd);
 
     return 0;
+}
+
+// Utility methods.
+int do_lseek(int fd, int offset, int whence)
+{
+    int ret = lseek(fd, offset, whence);
+    if (ret < 0)
+    {
+        perror("Failed to do lseek");
+        exit(1);
+    }
+    return ret;
 }
